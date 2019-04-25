51abfcf6d4d35c43947a0f23a3740c6484e7e46a
sadecvietnam <alex71hang@yahoo.se>
2019-04-25 16:19:39 +0200

    us2 och feature senaste

--- features/us2.feature
+++ features/us2.feature
@@ -1,12 +1,11 @@
 Feature: Swedenbank
 
-Scenario: I want to have my salary transfered 1000 kronor to my Lönekonto - 9170-843779
+Scenario: I want to have my salary transfered 22000 kronor to my Lönekonto - 1494-782136
 Given I have logged in as Arne
 And I am on the simulate-page
-When I enter my Lönekonto - 9170-843779 from dropdownmeny
+When I enter my Lönekonto "1494-782136" from dropdownmeny
 And I enter insättning from dropdownmeny
-And I enter an amount of 1000 kronor
-And I write a message - Lön 1000 kronor
+And I enter an amount of 22000 kronor
+And I write a message - Lön 22000 kronor
 And I click on the Utför button to transfer the money
-Then 1000 kronor is transfered to Arnes Lönekonto
-And check the amount after transaction
+Then 22000 kronor is transfered to Arnes Lönekonto
--- step-definitions/us2.js
+++ step-definitions/us2.js
@@ -6,123 +6,91 @@
 
 let {$, sleep} = require('./funcs');
 module.exports = function(){
+  
   let balanceBefore;
-    this.Given(/^I have logged in as Arne$/, async function () {
-        await helpers.loadPage('http://localhost:3000/#login');
-                  
-          driver.findElement(By.id("username")).sendKeys("Arne");   
-         driver.findElement(By.id("password")).sendKeys("222222"); 
-         await sleep(1000);
-         let LoginBtn= await driver.findElement(By.css('form.login-form button[type="submit"]'));
-         await LoginBtn.click();
-         await sleep(1000);
-          
-        
-        let guessLetters = await $('.username');
-        let contents = await guessLetters.getText();
-        // check that the pressedKey letter is included (as a capital)
-        console.warn(contents)
-        console.warn('Arne')
-        assert.equal(contents, 'Arne', 'Fel användarnamn') 
-       
-        await sleep(1000);
-        
-        // Goto the account page
-        await helpers.loadPage('http://localhost:3000/#my-accounts');
-        // Grab all lines with account info
-        let accounts = await $('section.accounts table tbody tr');
-        // Find the salary account ("Lönekonto")
-        let salaryAccount;
-        for(let account of accounts){
-          let text = await account.getText();
-          if(text.includes('Lönekonto')){
-            salaryAccount = account;
-          }
-        }
-        // td with balance
-        let balanceTd = await salaryAccount.findElement(by.css('td:nth-child(3)'));
-        let balance = await balanceTd.getText();
-        balance = balance.replace(/\D/g,'') / 100; // converting to number
-        //console.log("GOT THE BALANCE OF THE SALARY ACCOUNT BEFORE TRANSFER", balance);
-        balanceBefore = balance;
-          
-      });
+  
 
-      this.Given(/^I am on the simulate-page$/, async function () {
-        await helpers.loadPage('http://localhost:3000/#simulate');
+  this.Given(/^I have logged in as Arne$/, async function () {
+    await helpers.loadPage('http://localhost:3000/#login');
+              
+    await driver.findElement(By.id("username")).sendKeys("Arne");   
+    await driver.findElement(By.id("password")).sendKeys("222222"); 
+    await sleep(1000);
+    let LoginBtn= await driver.findElement(By.css('form.login-form button[type="submit"]'));
+    await LoginBtn.click();
+    await sleep(1000);
+      
+    
+    let guessLetters = await $('.username');
+    let contents = await guessLetters.getText();
+    // check that the pressedKey letter is included (as a capital)
+    console.warn(contents)
+    console.warn('Arne')
+    assert.equal(contents, 'Arne', 'Fel användarnamn') 
+   
+    await sleep(1000);
 
-      });
+    await helpers.loadPage('http://localhost:3000/#my-accounts');
+    // Grab all lines with account info
+    let accounts = await $('section.accounts table tbody tr');
+    let salaryAccount = await $('body > main > div > article > section.accounts.row.px-6 > table > tbody > tr:nth-child(1) > td.text-right');
+    let balance = await salaryAccount.getText()
+    balance = balance.replace(/\D/g,'') / 100; // converting to number
+    balanceBefore = balance;
+   
+      
+  });  
 
-      this.When(/^I enter my Lönekonto \- (\d+)\-(\d+) from dropdownmeny$/, async function (arg1, arg2) {
-        driver.findElement({id: 'accountNumber'});// select dropdown element you wish to select
-        await sleep(1000);
-        driver.findElement({id: 'accountNumber'}).sendKeys('Lönekonto - 9170-843779');
-        await sleep(1000);
-      });
+  this.Given(/^I am on the simulate-page$/, async function () {
+    await helpers.loadPage('http://localhost:3000/#simulate');
+    await sleep(2000);
+  });
 
-      this.When(/^I enter insättning from dropdownmeny$/, async function () {        
-        driver.findElement({id: 'depositOrWithdraw'});// select dropdown element you wish to select
-        await sleep(1000);
-        driver.findElement({id: 'depositOrWithdraw'}).sendKeys('Insättning');
-        await sleep(1000);
-      });
+  this.When(/^I enter my Lönekonto "([^"]*)" from dropdownmeny$/, async function (kontonr) {
+    let account = await $('#accountNumber > option:nth-child(1)')
+    await account.click()
+    await sleep(1000);
+  });
 
-      this.When(/^I enter an amount of (\d+) kronor$/, async function (arg1) {
-        driver.findElement({id: 'sum'});
-        await sleep(1000);
-        driver.findElement({id: 'sum'}).sendKeys('1000');
-        await sleep(1000);
-      });
+  this.When(/^I enter insättning from dropdownmeny$/, async function () {        
+    await driver.findElement({id: 'depositOrWithdraw'});// select dropdown element you wish to select
+    await sleep(1000);
+    await driver.findElement({id: 'depositOrWithdraw'}).sendKeys('Insättning');
+    await sleep(1000);
+  });
+  this.When(/^I enter an amount of (\d+) kronor$/, async function (arg1) {
+    driver.findElement({id: 'sum'});
+            await sleep(1000);
+            driver.findElement({id: 'sum'}).sendKeys('22000');
+            await sleep(2000);
+    
+  });
+  this.When(/^I write a message \- Lön (\d+) kronor$/, async function (arg1) {
+    await sleep(1000);
+    await driver.findElement(By.id("label")).sendKeys("Lön 22000 kronor"); 
+    await sleep(1000);
+  });
+  this.When(/^I click on the Utför button to transfer the money$/, async function () {
+    await sleep(1000);
+    let LoginBtn= await driver.findElement(By.css('form.simulate-form button[type="submit"]'));
+    await LoginBtn.click();
+    await sleep(1000);
+    //Så här gjorde vi innan
+    //driver.findElement(By.xpath("/html/body/main/div/article/form/div[5]/button")).click();
 
-      this.When(/^I write a message \- Lön (\d+) kronor$/, async function (arg1) {
-        await sleep(1000);
-        driver.findElement(By.id("label")).sendKeys("Lön 1000 kronor"); 
-        await sleep(1000);
-      });
+  });
 
-      this.When(/^I click on the Utför button to transfer the money$/, async function () {
-        await sleep(1000);
-        let LoginBtn= await driver.findElement(By.css('form.simulate-form button[type="submit"]'));
-        await LoginBtn.click();
-        await sleep(1000);
-        //Så här gjorde vi innan
-        //driver.findElement(By.xpath("/html/body/main/div/article/form/div[5]/button")).click();
-
-      });
-
-      this.Then(/^(\d+) kronor is transfered to Arnes Lönekonto$/, async function (arg1) {
-        await sleep(1000);
-        await helpers.loadPage('http://localhost:3000/#start');
-        await sleep(1000);
-      });
-
-      let balanceAfter;
-      this.Then(/^check the amount after transaction$/, async function(){
-        // Goto the account page
-        await helpers.loadPage('http://localhost:3000/#my-accounts');
-        // Grab all lines with account info
-        let accounts = await $('section.accounts table tbody tr');
-        let salaryAccount = await $('body > main > div > article > section.accounts.row.px-6 > table > tbody > tr:nth-child(1) > td.text-right');
-        let balance = await salaryAccount.getText()
-        //Benjamins ändringar
-        // Find the salary account ("Lönekonto")
-        // let salaryAccount;
-        // for(let account of accounts){
-        //   let text = await account.getText();
-        //   if(text.includes('Lönekonto')){
-        //     salaryAccount = account;
-        //   }
-        // }
-        // // td with balance
-        // let balanceTd = await salaryAccount.findElement(by.css('td:nth-child(3)'));
-        // let balance = await balanceTd.getText();
-        balance = balance.replace(/\D/g,'') / 100; // converting to number
-        console.log("GOT THE BALANCE OF THE SALARY ACCOUNT BEFORE TRANSFER", balanceBefore);
-        console.log("GOT THE BALANCE OF THE SALARY ACCOUNT AFTER TRANSFER", balance);
-        balanceAfter = balance;
-        console.log("GOT THE AMOUNT OF THE SALYRY:", balanceAfter - balanceBefore);
-
-      });
+  this.Then(/^(\d+) kronor is transfered to Arnes Lönekonto$/, async function (amount) {
+    await sleep(1000);
+    // Goto the account page
+    await helpers.loadPage('http://localhost:3000/#my-accounts');
+    // Grab all lines with account info
+    let accounts = await $('section.accounts table tbody tr');
+    let salaryAccount = await $('body > main > div > article > section.accounts.row.px-6 > table > tbody > tr:nth-child(1) > td.text-right');
+    let balance = await salaryAccount.getText()
+    balance = balance.replace(/\D/g,'') / 100; // converting to number
+    console.log("GOT THE AMOUNT OF THE SALYRY:", balance - balanceBefore);
+  });
 
 
 }
