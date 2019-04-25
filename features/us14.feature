60c2b494384ee6b0f0582a0c6250343d87f45ff4
PC9 <mercuryrising_xy@yahoo.com>
2019-04-25 21:51:34 +0200

    us14 nästsista

--- features/us14.feature
+++ features/us14.feature
@@ -4,9 +4,11 @@ Given I have logged in as Vanessa
 And I am on the start page
 When I choose transfer to other account than my own
 And I fill the account number "3091-228901"
-And I fill balance "20000" SEK
+And I fill balance "500" SEK
 And I press utför button
-Then I should see a message that i cannot transfer over in the last 7 days
+Then I should see a transfer approval message to approved
+And I choose again to other account than my own
+And I fill again the account number "3091-228901"
 And I change my balance "30001" SEK
-And I press utför button
-And I should see a transfer approval message
+And I press again utför button
+And I should see a message that i cannot transfer over in the last 7 days
--- step-definitions/us14.js
+++ step-definitions/us14.js
@@ -31,13 +31,12 @@ module.exports = function(){
   });
 
 
-  this.When(/^I fill balance "([^"]*)" SEK$/, async function (amount) {
+  this.When(/^I fill balance "([^"]*)" SEK$/, async function (under) {
 
-    let sumBalance= await driver.findElement(By.id("sum")).sendKeys(amount); 
+    let sumBalance= await driver.findElement(By.id("sum")).sendKeys(under); 
     await sleep (1000);
 
   });
-
   this.When(/^I press utför button$/,async function () {
     await sleep(1000);
         driver.findElement(By.xpath("/html/body/main/div/article/form/button")).click();
@@ -46,26 +45,48 @@ module.exports = function(){
          await sleep(2000);// Write code here that turns the phrase above into concrete actions
   });
 
-
-  this.Then(/^I should see a message that i cannot transfer over in the last (\d+) days$/,async function () {
-    await sleep(1000);
-    await driver.findElement(By.xpath("/html/body/main/div/article/form/button")).click();
-
+  this.Then(/^I should see a transfer approval message to approved$/,async function () {
+  await helpers.loadPage("http://localhost:3000/#my-accounts")   ;
+  await sleep (1000); 
   });
 
 
-  this.Then(/^I change my balance "([^"]*)" SEK$/, async function (under) {
-    let ChangeBalance= await driver.findElement(By.id("sum")).sendKeys(under); 
+
+  this.Then(/^I choose again to other account than my own$/,async function () {
+    await sleep(1000);
+    driver.findElement(By.xpath("/html/body/main/div/aside/nav/ul/li[5]/button/a")).click();
+    await sleep(1000);
+    
+    });
+
+ this.Then(/^I fill again the account number "([^"]*)"$/,async function (accnr) {
+      await sleep(1000);
+    await driver.findElement(By.xpath("//*[@id=\"accountTypes\"]/label[3]/input")).click(); //radio buttons
+    let accountNr = await driver.findElement(By.id("toAccountNumber")).sendKeys(accnr);
+    await sleep (1000);
+    });
+           
+           
+
+
+  this.Then(/^I change my balance "([^"]*)" SEK$/, async function (amount) {
+    let ChangeBalance= await driver.findElement(By.id("sum")).sendKeys(amount); 
     await sleep (1000);
             });
 
-   this.Then(/^I press utför button$/,async function () {
+   this.Then(/^I press again utför button$/,async function () {
+    await sleep(1000);
+    driver.findElement(By.xpath("/html/body/main/div/article/form/button")).click();
+    await sleep(1000); 
+     driver.switchTo().alert().accept();
+     await sleep(1000);
         
    });
 
-  this.Then(/^I should see a transfer approval message$/,async function () {
-  // Write code here that turns the phrase above into concrete actions
+   this.Then(/^I should see a message that i cannot transfer over in the last (\d+) days$/,async function () {
   
-});
+  });
+
+
    
 }
